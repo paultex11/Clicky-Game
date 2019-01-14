300ba6378a8279f008146bb0d22e5fe23efe04c8
Reaper1 <paultex11@gmail.com>
2019-01-14 17:01:41 -0600

    2nd commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+
+<footer className="footer bg-info">
+    <div className="container">
+      <span className="text-dark">Paul Aguilar Copyright 2019
+      </span>
+  </div>
+</footer>
+
+)
+export default Footer;
